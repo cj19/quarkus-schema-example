673f49add58fa0e2b01628f07d9a05ea1cd2462d
Roland Darvas <roland.darvas@khoch3.de>
2023-11-02 14:21:09 +0100

    Created a positive test case in AckServiceTest.java

--- src/test/java/service/AckServiceTest.java
+++ src/test/java/service/AckServiceTest.java
@@ -1,8 +1,5 @@
 package service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -13,7 +10,7 @@ import static org.hamcrest.Matchers.containsString;
 class AckServiceTest {
 
     @Test
-    void testAckService() {
+    void testAckServiceInvalidApduReasonCode() {
         RestAssured.given()
                 .header("Content-Type", "text/xml;charset=UTF-8")
                 .body("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ack=\"http://tsystems.com/etccm/iso12855/ack\"><soapenv:Header/><soapenv:Body><ack:InfoExchangeAck><ack:messageOriginator><ack:countryCode>1001010000</ack:countryCode><ack:providerIdentifier>40</ack:providerIdentifier></ack:messageOriginator><ack:ackADU><ack:apduAckCode>11</ack:apduAckCode></ack:ackADU></ack:InfoExchangeAck></soapenv:Body></soapenv:Envelope>")
@@ -22,5 +19,16 @@ class AckServiceTest {
                 .then()
                 .statusCode(500)
                 .body(containsString("Unmarshalling Error: cvc-maxInclusive-valid: Value '11' is not facet-valid with respect to maxInclusive '10' for type 'ApduReasonCode'."));
+    }
+
+    @Test
+    void testAckServiceValidXml() {
+        RestAssured.given()
+                .header("Content-Type", "text/xml;charset=UTF-8")
+                .body("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ack=\"http://tsystems.com/etccm/iso12855/ack\"><soapenv:Header/><soapenv:Body><ack:InfoExchangeAck><ack:messageOriginator><ack:countryCode>1001010000</ack:countryCode><ack:providerIdentifier>40</ack:providerIdentifier></ack:messageOriginator><ack:ackADU><ack:apduAckCode>10</ack:apduAckCode></ack:ackADU></ack:InfoExchangeAck></soapenv:Body></soapenv:Envelope>")
+                .when()
+                .post("/test/service/ackService")
+                .then()
+                .statusCode(200);
     }
 }
